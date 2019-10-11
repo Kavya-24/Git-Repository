df443dcfd0323d6ab0b23e979df66f44f4982635
Kavya-24 <kavya-1901cs30@iitp.ac.in>
2019-10-11 16:07:35 +0530

    add the code file

---
+++ textCode.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    //Lets print our name, and upload it in GIT repository
+    string name;
+    cin>>name;
+    cout<<"My name is "<<name<<endl;
+    return 0;
+}
